e583164b04505c56f9c536983ad75a90d0812e2f
Manuel Cobos <flipper7247@gmail.com>
2025-09-19 17:01:58 -0500

    Agregando middleware para estandarizar respuestas del API

---
+++ src/middlewares/response.middleware.js
@@ -0,0 +1,22 @@
+export function responseMiddleware(req, res, next) {
+  // sobreescribimos res.success y res.error para unificar respuestas
+  res.success = (data, message = "Operación exitosa", code = 200) => {
+    res.status(code).json({
+      status: "success",
+      code,
+      message,
+      obj: data, // aquí entra lo que devuelva tu controlador
+    });
+  };
+
+  res.error = (message = "Error en el servidor", code = 500, details = null) => {
+    res.status(code).json({
+      status: "error",
+      code,
+      message,
+      obj: details,
+    });
+  };
+
+  next();
+}
