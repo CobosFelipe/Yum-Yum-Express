70cd0e4f2a6a455f2a294bb5df8d5fab45a6cc3b
Manuel Cobos <flipper7247@gmail.com>
2025-09-20 00:47:29 -0500

    Actualización para validar parametros

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -10,7 +10,24 @@ export function validateSchema(schema) {
       );
     }
 
-    req.body = value; // datos validados
+    req.body = value;
+    next();
+  };
+}
+
+export function validateParams(schema) {
+  return (req, res, next) => {
+    const { error, value } = schema.validate(req.params, { abortEarly: false });
+
+    if (error) {
+      return res.error(
+        "Datos inválidos",
+        400,
+        error.details.map((d) => d.message)
+      );
+    }
+
+    req.params = value;
     next();
   };
 }
