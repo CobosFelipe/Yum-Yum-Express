460f7527afb13e5a1d21926fc0d01b7929176857
Manuel Cobos <flipper7247@gmail.com>
2025-09-20 00:41:29 -0500

    Esquema para el manejo de datos de los usuarios

---
+++ src/schemas/user.schema.js
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+// Esquema para creación de usuario
+export const createUserSchema = Joi.object({
+  name: Joi.string().min(3).max(25).required(),
+  email: Joi.string().email().max(50).required(),
+  telephone: Joi.string()
+    .pattern(/^[36]\d{9}$/)
+    .required()
+    .messages({
+      "string.pattern.base": "El número de teléfono debe tener 10 dígitos y empezar con 3 o 6."
+    }),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{8,20}$")).required(),
+});
+
+// Esquema para validar usuario
+export const validateUserSchema = Joi.object({
+  email: Joi.string().email().max(50).required(),
+  password: Joi.string().min(8).max(20).required(),
+});
