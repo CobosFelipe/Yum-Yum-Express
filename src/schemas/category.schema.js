4bd2f93e61264e13dafafaf4ff12b0024dffbdb4
Manuel Cobos <flipper7247@gmail.com>
2025-09-20 02:03:29 -0500

    Definiendo esquemas para las categorias

---
+++ src/schemas/category.schema.js
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+// Esquema para crear una categoria
+export const addCategorySchema = Joi.object({
+  name: Joi.string().min(5).max(50).required(),
+});
+
+// Esquema para editar una categoria
+export const editCategorySchema = Joi.object({
+  category_id: Joi.number().integer().min(1).required(),
+  name: Joi.string().min(5).max(50).required(),
+});
+
+// Esquema para eliminar una categoria
+export const deleteCategorySchema = Joi.object({
+  category_id: Joi.number().integer().min(1).required(),
+});
