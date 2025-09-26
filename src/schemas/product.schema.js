c98b1a9ccb58628b34c08a1828312a70097f7a2e
Manuel Cobos <flipper7247@gmail.com>
2025-09-26 06:35:55 -0500

    Actualización de esquema, mas datos requeridos

--- src/schemas/product.schema.js
+++ src/schemas/product.schema.js
@@ -3,8 +3,8 @@ import Joi from "joi";
 // Esquema para crear o editar un producto
 export const mainProductSchema = Joi.object({
   product_id: Joi.number().integer().min(1),
-  fk_category_id: Joi.number().integer().max(50),
-  name: Joi.string().min(5).max(50).required(),
+  fk_category_id: Joi.number().integer().max(50).required(),
+  name: Joi.string().min(5).max(50).required().required(),
   price: Joi.number().integer().min(100).max(1000000).required(),
   description: Joi.string().max(255).required(),
   img: Joi.string().max(255).required(),
