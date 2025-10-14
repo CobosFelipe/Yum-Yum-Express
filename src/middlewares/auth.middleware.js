cd45653210bf6cf3eeb124ab6c06698590e82025
Manuel Cobos <flipper7247@gmail.com>
2025-10-14 13:06:47 -0500

    Agregando middlewarepara autenticacion de usuario no admin

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
-export default function requireAuth (req, res, next) {
+export function requireAuth(req, res, next) {
   // Obtener el token de la cookie
   const token = req.cookies.acces_token;
   // Obtener el jwt_key de las variables de entorno
   const JWT_KEY = process.env.SECRET_JWT_KEY;
 
-  if (!token ) {
+  if (!token) {
     return res.error("Acceso denegado", 401);
   }
 
@@ -16,10 +16,35 @@ export default function requireAuth (req, res, next) {
     if (!data.isAdmin) {
       return res.error("Usuario debe ser admin", 401);
     }
-        
+
     // Adjuntar los datos del usuario a la solicitud
     req.user = data;
-    
+
+    next();
+  } catch (error) {
+    console.log("Error");
+    // Si el token es invalido o expiró
+    return res.error("Token inválido o expirado.", 403);
+  }
+}
+
+export function requireLogin(req, res, next) {
+  // Obtener el token de la cookie
+  const token = req.cookies.acces_token;
+  // Obtener el jwt_key de las variables de entorno
+  const JWT_KEY = process.env.SECRET_JWT_KEY;
+
+  if (!token) {
+    return res.error("Acceso denegado, debes iniciar sesión", 401);
+  }
+
+  try {
+    // Verificar el token
+    const data = jwt.verify(token, JWT_KEY);
+
+    // Adjuntar los datos del usuario a la solicitud
+    req.user = data;
+
     next();
   } catch (error) {
     console.log("Error");
